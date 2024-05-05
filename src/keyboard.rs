ed75c0471f71a10dde83f6dd64a2c18e3f3ff87c
podskio <podskio1@gmail.com>
2024-05-04 20:58:33 -0700

    Add keyboard handling

---
+++ src/keyboard.rs
@@ -0,0 +1,51 @@
+use crate::{
+    game::{environment_at_position, GameState},
+    GAME_SIZE,
+};
+use crossterm::event::KeyCode;
+
+pub fn handle_game_input(state: &mut GameState, key: KeyCode) {
+    match key {
+        KeyCode::Char('w') | KeyCode::Up => {
+            if state.player.position.1 > 0
+                && !environment_at_position(
+                    (state.player.position.0, state.player.position.1 - 1),
+                    &state,
+                )
+            {
+                state.player.position.1 -= 1;
+            }
+        }
+        KeyCode::Char('s') | KeyCode::Down => {
+            if state.player.position.1 < GAME_SIZE - 1
+                && !environment_at_position(
+                    (state.player.position.0, state.player.position.1 + 1),
+                    &state,
+                )
+            {
+                state.player.position.1 += 1;
+            }
+        }
+        KeyCode::Char('a') | KeyCode::Left => {
+            if state.player.position.0 > 0
+                && !environment_at_position(
+                    (state.player.position.0 - 1, state.player.position.1),
+                    &state,
+                )
+            {
+                state.player.position.0 -= 1;
+            }
+        }
+        KeyCode::Char('d') | KeyCode::Right => {
+            if state.player.position.0 < GAME_SIZE - 1
+                && !environment_at_position(
+                    (state.player.position.0 + 1, state.player.position.1),
+                    &state,
+                )
+            {
+                state.player.position.0 += 1;
+            }
+        }
+        _ => {}
+    }
+}
